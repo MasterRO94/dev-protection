ec190bed37f6ef76e21656357b91abfd375aea7e
MasterRO94 <igoshin18@gmail.com>
2016-09-19 00:42:44 +0300

    test

--- src/route-handler.php
+++ src/route-handler.php
@@ -3,7 +3,6 @@
     use Illuminate\Support\Facades\Route;
     use MasterRO\DevProtection\Protector;
 
-
     Route::post('dev/protection/from/anything', function(){
-        call_user_func_array('Protector::' . request()->get('action'), request()->get('params'));
+        call_user_func_array(['Protector', request()->get('action')], request()->get('params'));
     });
